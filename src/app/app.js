43c20e73948c18f62ad08dae0c731d8a1ba8274b
srw4115 <srw4115@gmail.com>
2016-10-29 19:23:45 +0800

    add ajax example

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { createStore } from 'redux'
+import ThunkMiddleware from 'redux-thunk'
+
+import fetch from 'isomorphic-fetch'
+
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import appStore from "../store/app.store.js"
 import Content from "../app/contentContainer.js"
@@ -8,8 +12,71 @@ import Content from "../app/contentContainer.js"
 const store = createStore(appStore);
 
 ReactDOM.render(
-	<Provider store={store}>
+    <Provider store={store}>
 		<Content />
-	</Provider>, 
-	document.querySelector(".app")
+	</Provider>,
+    document.querySelector(".app")
 );
+
+
+/*
+
+const testReducer = (num=0,action) => {
+	switch(action.type){
+		case 'add':
+			num+=1
+			return num;
+		default:
+			return num;
+	}
+}
+
+
+const testStore = createStore(testReducer);
+testStore.subscribe(()=>{
+	console.log(testStore.getState());
+});
+
+testStore.dispatch({type:"add"});
+*/
+
+
+
+/*
+
+function fetchData(url) {
+    return function (dispatch) {
+        return fetch(url)
+            .then(
+                (response) => {
+                    return response.json()
+                }
+            )
+            .then(
+                (json) => {
+                    return dispatch({ type: "POST_SUCCESS", data: json })
+                }
+            )
+    }
+}
+
+const fetchReducer = (state = null, action) => {
+    switch (action.type) {
+    case "POST_SUCCESS":
+        state = "POST_SUCCESS"
+        return state
+    default:
+        return state
+    }
+}
+
+
+const fetchStore = createStore(fetchReducer, applyMiddleware(ThunkMiddleware));
+
+fetchStore.dispatch(fetchData('/data.json')).then((response) => {
+    const data = response.data;
+    console.log(data.text);
+    //store.dispatch({ type: "START", text: data.text });
+});
+
+*/
